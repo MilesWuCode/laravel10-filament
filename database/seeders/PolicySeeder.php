321a4774fa54683c676c8b5cfaf489a129026a5e
Miles Wu <slion0725@gmail.com>
2023-08-29 15:15:13 +0800

    upd

--- database/seeders/PolicySeeder.php
+++ database/seeders/PolicySeeder.php
@@ -14,22 +14,24 @@ class PolicySeeder extends Seeder
      */
     public function run(): void
     {
+        $name = '用戶';
+
         $guardName = config('auth.defaults.guard');
 
         // 常用:viewAny,view,create,update,delete
-        Permission::firstOrCreate(['name' => '用戶-任意檢視', 'guard_name' => $guardName]);
-        Permission::firstOrCreate(['name' => '用戶-檢視', 'guard_name' => $guardName]);
-        Permission::firstOrCreate(['name' => '用戶-新增', 'guard_name' => $guardName]);
-        Permission::firstOrCreate(['name' => '用戶-更新', 'guard_name' => $guardName]);
-        Permission::firstOrCreate(['name' => '用戶-刪除', 'guard_name' => $guardName]);
+        Permission::firstOrCreate(['name' => $name.'-任意檢視', 'guard_name' => $guardName]);
+        Permission::firstOrCreate(['name' => $name.'-檢視', 'guard_name' => $guardName]);
+        Permission::firstOrCreate(['name' => $name.'-新增', 'guard_name' => $guardName]);
+        Permission::firstOrCreate(['name' => $name.'-更新', 'guard_name' => $guardName]);
+        Permission::firstOrCreate(['name' => $name.'-刪除', 'guard_name' => $guardName]);
 
         // 不常使用:forceDelete,restore
-        Permission::firstOrCreate(['name' => '用戶-強制刪除', 'guard_name' => $guardName]);
-        Permission::firstOrCreate(['name' => '用戶-還原刪除', 'guard_name' => $guardName]);
+        Permission::firstOrCreate(['name' => $name.'-強制刪除', 'guard_name' => $guardName]);
+        Permission::firstOrCreate(['name' => $name.'-還原刪除', 'guard_name' => $guardName]);
 
         // 特別:deleteAny,restoreAny,forceDeleteAny
-        Permission::firstOrCreate(['name' => '用戶-任意刪除', 'guard_name' => $guardName]);
-        Permission::firstOrCreate(['name' => '用戶-任意強制刪除', 'guard_name' => $guardName]);
-        Permission::firstOrCreate(['name' => '用戶-任意還原刪除', 'guard_name' => $guardName]);
+        Permission::firstOrCreate(['name' => $name.'-任意刪除', 'guard_name' => $guardName]);
+        Permission::firstOrCreate(['name' => $name.'-任意強制刪除', 'guard_name' => $guardName]);
+        Permission::firstOrCreate(['name' => $name.'-任意還原刪除', 'guard_name' => $guardName]);
     }
 }
