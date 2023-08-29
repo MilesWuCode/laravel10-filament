fea8868da1c5703b473917a48133e8760388e441
Miles Wu <slion0725@gmail.com>
2023-08-29 14:09:04 +0800

    upd

--- database/seeders/PolicySeeder.php
+++ database/seeders/PolicySeeder.php
@@ -14,8 +14,29 @@ class PolicySeeder extends Seeder
     {
         $guardName = config('auth.defaults.guard');
 
+        // viewAny
+        // view
+        // create
+        // update
+        // delete
+        // deleteAny
+        // restore
+        // restoreAny
+        // forceDelete
+        // forceDeleteAny
+        Permission::firstOrCreate(['name' => '管理員-任意檢視', 'guard_name' => $guardName]);
+        Permission::firstOrCreate(['name' => '管理員-檢視', 'guard_name' => $guardName]);
         Permission::firstOrCreate(['name' => '管理員-新增', 'guard_name' => $guardName]);
         Permission::firstOrCreate(['name' => '管理員-更新', 'guard_name' => $guardName]);
         Permission::firstOrCreate(['name' => '管理員-刪除', 'guard_name' => $guardName]);
+
+        // 不常使用
+        // Permission::firstOrCreate(['name' => '管理員-強制刪除', 'guard_name' => $guardName]);
+        // Permission::firstOrCreate(['name' => '管理員-還原刪除', 'guard_name' => $guardName]);
+
+        // 特別
+        // Permission::firstOrCreate(['name' => '管理員-任意刪除', 'guard_name' => $guardName]);
+        // Permission::firstOrCreate(['name' => '管理員-任意強制刪除', 'guard_name' => $guardName]);
+        // Permission::firstOrCreate(['name' => '管理員-任意還原刪除', 'guard_name' => $guardName]);
     }
 }
